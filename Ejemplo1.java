a7f0dbaf730e8af2aaf9b51a25c5ed5a1c10e0ae
Marcos <Marcos@gmail.com>
2024-02-22 21:35:02 +0100

    ejemplo1 b

--- Ejemplo1.java
+++ Ejemplo1.java
@@ -3,7 +3,8 @@ package proyectoGitHub;
 public class Ejemplo1 {
 
 	public static void main(String[] args) {
-		System.out.println("hola");
+		System.out.println("a");
+		System.out.println("b");
 
 	}
 
