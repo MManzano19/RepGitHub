151e701d3787bc3d40f363f773c5df4ca9994292
Marcos <Marcos@gmail.com>
2024-02-25 21:47:42 +0100

    ramita

--- Ejemplo2.java
+++ Ejemplo2.java
@@ -5,7 +5,7 @@ public class Ejemplo2 {
 	public static void main(String[] args) {
 		System.out.println("a");
 		System.out.println("b");
-System.out.println("a");
+		System.out.println("a");
 	}
 
 }
